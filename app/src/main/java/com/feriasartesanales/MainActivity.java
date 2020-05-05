581e348a772b70fa7c18bea942db873e89404bdc
taielmartinez <taielmartinezgort@gmail.com>
2020-05-05 10:59:30 -0300

    Aplicacion de pulsaciones basica de pruebas completada, problema con el rescalado de ui

--- app/src/main/java/com/feriasartesanales/MainActivity.java
+++ app/src/main/java/com/feriasartesanales/MainActivity.java
@@ -8,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,10 +16,34 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public int count;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        count = 0;
+
+    }
+
+    public void count_sum(View view){
+        count++;
+        count_text();
+    }
+
+    public void count_res(View view){
+        count--;
+        count_text();
+    }
+    public void count_reset(View view){
+        count = 0;
+        count_text();
+    }
+
+    public void count_text(){
+        TextView count_text=(TextView)findViewById(R.id.contador);
+        count_text.setText("Cdor: " + count);
 
     }
 
